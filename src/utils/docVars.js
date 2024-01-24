26d452e86537c51e90f4de24b69f2d6d190f92ba
Mikey Nichols <mikenichols410@gmail.com>
2024-01-24 16:04:41 -0500

    refactors and also exports functions within docVars

--- src/utils/docVars.js
+++ src/utils/docVars.js
@@ -4,19 +4,13 @@ export const main = document.querySelector("main");
 export const footer = document.querySelector("footer");
 
 /* Function to empty the content of the header element */
-function clearHeader() {
-  header.innerHTML = "";
-}
+export const clearHeader = () => (header.innerHTML = "");
 
 /* Function to empty the content of the main element */
-function clearMain() {
-  main.innerHTML = "";
-}
+export const clearMain = () => (main.innerHTML = "");
 
 /* Function to empty the content of the footer element */
-function clearFooter() {
-  footer.innerHTML = "";
-}
+export const clearFooter = () => (footer.innerHTML = "");
 
 export const clearEle = (ele) => (ele.innerHTML = "");
 
