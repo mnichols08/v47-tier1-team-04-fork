4979b8a6bd170eb7ae139fa280361e42159f7a82
Mikey Nichols <mikenichols410@gmail.com>
2024-01-24 15:31:10 -0500

    creates a docVars utility file, for clearing specific  parts of the document as well as a function to clear any elements

---
+++ src/utils/docVars.js
@@ -0,0 +1,23 @@
+/* as recommend, anchored everything to the <main>, so if we declare the variable it no longer matters it is just defined */
+export const header = document.querySelector("header");
+export const main = document.querySelector("main");
+export const footer = document.querySelector("footer");
+
+/* Function to empty the content of the header element */
+function clearHeader() {
+  header.innerHTML = "";
+}
+
+/* Function to empty the content of the main element */
+function clearMain() {
+  main.innerHTML = "";
+}
+
+/* Function to empty the content of the footer element */
+function clearFooter() {
+  footer.innerHTML = "";
+}
+
+export const clearEle = (ele) => (ele.innerHTML = "");
+
+export default clearAnchor;
