7a53e68c8cb9d9bbf3a8c8834ac3cbdfe435a422
Mikey Nichols <mikenichols410@gmail.com>
2024-01-24 15:18:31 -0500

    moves script to the bottom of body element

--- dist/main.js
+++ dist/main.js
@@ -7,26 +7,73 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: C:\\Code\\chingu\\tasks-fork\\src\\index.js: Support for the experimental syntax 'importAttributes' isn't currently enabled (1:46):\n\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 1 |\u001b[39m \u001b[36mimport\u001b[39m stories \u001b[36mfrom\u001b[39m \u001b[32m'./scripts/stories.json'\u001b[39m assert {type\u001b[33m:\u001b[39m \u001b[32m'json'\u001b[39m}\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m   |\u001b[39m                                              \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\nAdd @babel/plugin-syntax-import-attributes (https://github.com/babel/babel/tree/main/packages/babel-plugin-syntax-import-attributes) to the 'plugins' section of your Babel config to enable parsing.\n    at constructor (C:\\Code\\chingu\\tasks-fork\\node_modules\\@babel\\parser\\lib\\index.js:356:19)\n    at Parser.raise (C:\\Code\\chingu\\tasks-fork\\node_modules\\@babel\\parser\\lib\\index.js:3223:19)\n    at Parser.expectOnePlugin (C:\\Code\\chingu\\tasks-fork\\node_modules\\@babel\\parser\\lib\\index.js:3269:18)\n    at Parser.maybeParseImportAttributes (C:\\Code\\chingu\\tasks-fork\\node_modules\\@babel\\parser\\lib\\index.js:14272:14)\n    at Parser.parseImportSourceAndAttributes (C:\\Code\\chingu\\tasks-fork\\node_modules\\@babel\\parser\\lib\\index.js:14160:10)\n    at Parser.parseImportSpecifiersAndAfter (C:\\Code\\chingu\\tasks-fork\\node_modules\\@babel\\parser\\lib\\index.js:14154:17)\n    at Parser.parseImport (C:\\Code\\chingu\\tasks-fork\\node_modules\\@babel\\parser\\lib\\index.js:14145:17)\n    at Parser.parseStatementContent (C:\\Code\\chingu\\tasks-fork\\node_modules\\@babel\\parser\\lib\\index.js:12710:27)\n    at Parser.parseStatementLike (C:\\Code\\chingu\\tasks-fork\\node_modules\\@babel\\parser\\lib\\index.js:12588:17)\n    at Parser.parseModuleItem (C:\\Code\\chingu\\tasks-fork\\node_modules\\@babel\\parser\\lib\\index.js:12565:17)\n    at Parser.parseBlockOrModuleBlockBody (C:\\Code\\chingu\\tasks-fork\\node_modules\\@babel\\parser\\lib\\index.js:13189:36)\n    at Parser.parseBlockBody (C:\\Code\\chingu\\tasks-fork\\node_modules\\@babel\\parser\\lib\\index.js:13182:10)\n    at Parser.parseProgram (C:\\Code\\chingu\\tasks-fork\\node_modules\\@babel\\parser\\lib\\index.js:12464:10)\n    at Parser.parseTopLevel (C:\\Code\\chingu\\tasks-fork\\node_modules\\@babel\\parser\\lib\\index.js:12454:25)\n    at Parser.parse (C:\\Code\\chingu\\tasks-fork\\node_modules\\@babel\\parser\\lib\\index.js:14376:10)\n    at parse (C:\\Code\\chingu\\tasks-fork\\node_modules\\@babel\\parser\\lib\\index.js:14417:38)\n    at parser (C:\\Code\\chingu\\tasks-fork\\node_modules\\@babel\\core\\lib\\parser\\index.js:41:34)\n    at parser.next (<anonymous>)\n    at normalizeFile (C:\\Code\\chingu\\tasks-fork\\node_modules\\@babel\\core\\lib\\transformation\\normalize-file.js:64:37)\n    at normalizeFile.next (<anonymous>)\n    at run (C:\\Code\\chingu\\tasks-fork\\node_modules\\@babel\\core\\lib\\transformation\\index.js:21:50)\n    at run.next (<anonymous>)\n    at transform (C:\\Code\\chingu\\tasks-fork\\node_modules\\@babel\\core\\lib\\transform.js:22:33)\n    at transform.next (<anonymous>)\n    at step (C:\\Code\\chingu\\tasks-fork\\node_modules\\gensync\\index.js:261:32)\n    at C:\\Code\\chingu\\tasks-fork\\node_modules\\gensync\\index.js:273:13\n    at async.call.result.err.err (C:\\Code\\chingu\\tasks-fork\\node_modules\\gensync\\index.js:223:11)\n    at C:\\Code\\chingu\\tasks-fork\\node_modules\\gensync\\index.js:189:28\n    at C:\\Code\\chingu\\tasks-fork\\node_modules\\@babel\\core\\lib\\gensync-utils\\async.js:67:7\n    at C:\\Code\\chingu\\tasks-fork\\node_modules\\gensync\\index.js:113:33\n    at step (C:\\Code\\chingu\\tasks-fork\\node_modules\\gensync\\index.js:287:14)\n    at C:\\Code\\chingu\\tasks-fork\\node_modules\\gensync\\index.js:273:13\n    at async.call.result.err.err (C:\\Code\\chingu\\tasks-fork\\node_modules\\gensync\\index.js:223:11)\n    at C:\\Code\\chingu\\tasks-fork\\node_modules\\gensync\\index.js:37:40");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _model_tasks_json__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./model/tasks.json */ \"./src/model/tasks.json\");\n\n\n// document.querySelector('main').innerHTML = INITIAL_DATA//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW5kZXguanMiLCJtYXBwaW5ncyI6Ijs7QUFBa0U7O0FBRWxFIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdjQ3LXRpZXIxLXRlYW0tMDQtZm9yay8uL3NyYy9pbmRleC5qcz9iNjM1Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBJTklUSUFMX0RBVEEgZnJvbSAnLi9tb2RlbC90YXNrcy5qc29uJyBhc3NlcnQge3R5cGU6ICdqc29uJ307XHJcblxyXG4vLyBkb2N1bWVudC5xdWVyeVNlbGVjdG9yKCdtYWluJykuaW5uZXJIVE1MID0gSU5JVElBTF9EQVRBIl0sIm5hbWVzIjpbIklOSVRJQUxfREFUQSIsInR5cGUiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/index.js\n");
+
+/***/ }),
+
+/***/ "./src/model/tasks.json":
+/*!******************************!*\
+  !*** ./src/model/tasks.json ***!
+  \******************************/
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('[{"categoryName":"ROUTINE ACTIVITIES","activityTypes":[{"activityName":"Projects","Tasks":[{"taskName":"Update Recipes Project Backlog","taskDescription":"","days":["monday"]},{"taskName":"Update The dailyTasks sheet with the backlog tasks","taskDescription":"add the filtering feature to Done","days":["monday"]}]},{"activityName":"Blog Posts","Tasks":[{"taskName":"Publish The recent Blog Post Draft to hashnode","taskDescription":"","days":["friday"]},{"taskName":"Write a New headline in a Blog Post Draft","taskDescription":"","days":["saturday","sunday","monday","tuesday","wednesday","thursday","friday"]}]}]},{"categoryName":"STUDYING","activityTypes":[{"activityName":"Node Js Course","Tasks":[{"taskName":"Plan The Node Js Course Progress By Month","taskDescription":"Set Up A Plan For The Next Month Of Node Js Learning","days":["1"]},{"taskName":"Study The First Node Js Lecture","taskDescription":"","days":["2"]}]},{"activityName":"MongoDB","Tasks":[{"taskName":"Plan The MongoDB Course Progress By Month","taskDescription":"Set Up A Plan For The Next Month Of Node Js Learning","days":["30"]},{"taskName":"Study The First MongoDB Lecture","taskDescription":"","days":["15"]}]}]},{"categoryName":"DAILY TASKS PROJECT","activityTypes":[{"activityName":"Backlog","Tasks":[{"taskName":"Add The New Features list","taskDescription":"","days":["monday"]},{"taskName":"Add The New PRs To InReview","taskDescription":"","days":["7"]}]},{"activityName":"Coding","Tasks":[{"taskName":"Work On The Sidebar","taskDescription":"Add The Sections Links","days":["thursday"]},{"taskName":"Refactor The Filtering Feature Code","taskDescription":"","days":["friday"]}]}]},{"categoryName":"CHINGU","activityTypes":[{"activityName":"Voyage","Tasks":[{"taskName":"Conduct The Project Planning Meeting","taskDescription":"Conduct The Project Planning Meeting To Discuss Our Ideas","days":["monday"]},{"taskName":"Create The UI/UX Design For The DailyTasks Project","taskDescription":"Create The UI/UX Design For The DailyTasks Project Based On The Team Discussion","days":["monday","tuesday","wednesday"]}]},{"activityName":"Pair Programming","Tasks":[{"taskName":"Create When2Meet Link","taskDescription":"Create When2Meet Link To Introduce Yourselves","days":["thursday"]},{"taskName":"Attend The Introduction Meeting With Someone","taskDescription":"","days":["thursday"]}]}]}]');
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
