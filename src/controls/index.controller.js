35f3d61bcd9818367d64e5f49cd0980ad26a180f
Mikey Nichols <mikenichols410@gmail.com>
2024-01-24 16:26:44 -0500

    moves initial data import into a new file called index.controller

---
+++ src/controls/index.controller.js
@@ -0,0 +1,4 @@
+// Imports JSON data from the "model"
+import INITIAL_DATA from './models/tasks.model.json' assert {type: 'json'};
+// Logs this value to console
+console.log(INITIAL_DATA);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
-import './styles/index.scss'; // declares stylesheet
+import './styles/index.scss'; // imports primary stylesheet
 
-// Imports JSON data from the "model"
-import INITIAL_DATA from './models/tasks.model.json' assert {type: 'json'};
-// Logs this value to console
-console.log(INITIAL_DATA);
