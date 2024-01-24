66abefb07d71533c198c6725f475791136b56ae9
Mikey Nichols <mikenichols410@gmail.com>
2024-01-24 15:38:12 -0500

    imports scss file into js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './styles/index.scss'; // declares stylesheets that rely on images we need to render on screen from within our server
+
 // Imports JSON data from the "model"
 import INITIAL_DATA from './model/tasks.json' assert {type: 'json'};
 // Logs this value to console
