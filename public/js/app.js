f2339333c8e7703529f91ccde998ca156f255394
Mikey Nichols <mnix@journeytocode.io>
2024-01-31 17:42:27 -0500

    restores app init to simplicity

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,5 @@
 import app from "./model/model.js";
 
 /**************************APP INITIALIZATION************************** */
+app.init("Daily Task App"); // Initialize application with a title of Daily Task App
 
-(async function () {
-  // Initialize application with a title of Daily Task App
-  await app.init("Daily Task App");
-
-})();
